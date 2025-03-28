f82b6a934d3386201489ea3035fc6421cc80d1b9
Svetlana Agafonova <sveta_tokiohotel@mail.ru>
2025-03-28 19:16:26 +0500

    Pr10_first

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -11,59 +11,6 @@ import { ArticleStateType, OptionType, defaultArticleState } from 'src/constants
 import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr } from 'src/constants/articleProps';
 import clsx from 'clsx';
 
-// interface ArticleParamsFormProps {
-// 	isOpen: boolean
-// 	onArrowButtonClick: () => void
-// 	// onApply: (state: ArticleStateType) => void
-// }
-
-// export const ArticleParamsForm = ({isOpen, onArrowButtonClick}: ArticleParamsFormProps) => {
-// 	const [sidebarState, setSiderbarState] = useState<ArticleStateType>(defaultArticleState)
-
-// 	const handleOptionChange = (option: string, value: OptionType) => {
-// 		setSiderbarState({...sidebarState, [option]: value})
-// 	}
-
-// 	const handleArrowButtonClick = (event: MouseEvent) => {
-// 		onArrowButtonClick()
-// 	}
-
-// 	const handleAsideClick = (event: MouseEvent) => {
-// 		event.stopPropagation()
-// 	}
-
-// 	// const handleApply = (event: MouseEvent) => {
-// 	// 	onApply(sidebarState)
-// 	// }
-
-// 	return (
-// 		<>
-// 			<ArrowButton isOpen={isOpen} onClick={handleArrowButtonClick} />
-// 			<aside className={clsx(styles.container, isOpen && styles.container_open)} onClick={handleAsideClick}>
-// 				<form className={styles.form}>
-// 					<Text uppercase={true} weight={800} size={31}> задайте параметры
-// 					</Text>
-// 					<Select options={fontFamilyOptions} selected={sidebarState.fontFamilyOption} onChange={(value) => handleOptionChange('fontFamilyOption', value)} title = 'шрифт'
-// 					/>
-// 					<RadioGroup name='fontSizeOptions' options={fontSizeOptions} selected={sidebarState.fontSizeOption} onChange={(value) => handleOptionChange('fontSizeOption', value)} title='размер шрифта'
-// 					/>
-// 					<Select options={fontColors} selected={sidebarState.fontColor} onChange={(value) => handleOptionChange('fontColor', value)} title = 'цвет шрифта'
-// 					/>
-// 					<Separator
-// 					/>
-// 					<Select options={backgroundColors} selected={sidebarState.backgroundColor} onChange={(value) => handleOptionChange('backgroundColor', value)} title = 'цвет фона'
-// 					/>
-// 					<Select options={contentWidthArr} selected={sidebarState.contentWidth} onChange={(value) => handleOptionChange('contentWidth', value)} title = 'ширина контента'
-// 					/>
-// 					<div className={styles.bottomContainer}>
-// 						<Button title='Сбросить' htmlType='reset' type='clear' />
-// 						<Button title='Применить' htmlType='submit' type='apply' />
-// 					</div>
-// 				</form>
-// 			</aside>
-// 		</>
-// 	);
-// };
 interface IArticleParamsFormProps {
 	onSubmit: (state: ArticleStateType) => void;
 }
